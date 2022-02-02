e1aefe25da3ffebba7c16c0e4ec0a12fe876093b
ArsenyBuntury <yeahwheel@gmail.com>
2022-02-02 22:13:06 +0300

    wip

--- app/controllers/twits_controller.rb
+++ app/controllers/twits_controller.rb
@@ -1,5 +1,26 @@
 class TwitsController < ApplicationController
 	def index
-		
+		@twits=Twit.all
 	end
+
+	def new
+		@twit=Twit.new
+	end
+
+	def show
+		@twit=Twit.find(params[:id])
+	end
+
+	def create
+		@twit= Twit.new(name:"...",body:"...")
+		if @twit.save
+			redirect_to @twit
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+	#private def twit_params
+		#params.require(:twit).permit(:name, :body)
+	#end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'twits#index'
   get 'about'=>'pages#about'
   get 'contacts'=>'pages#contacts'
+
+  resources :twits
 end
