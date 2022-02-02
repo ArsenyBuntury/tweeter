a2e2ed2573518c72b249370fa8d558ebbc233399
ArsenyBuntury <yeahwheel@gmail.com>
2022-02-02 11:07:44 +0300

    added pages controller and create some static pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+	def about
+		@aboutus='Всем привет! Я начинающий руби разработчик. Вы находитесь на моем клоне твиттера.'
+	end
+
+	def contacts
+
+	end
+
+end
---
+++ app/controllers/twits_controller.rb
@@ -0,0 +1,5 @@
+class TwitsController < ApplicationController
+	def index
+		
+	end
+end
