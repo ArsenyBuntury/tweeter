c763f6f1ce537ac2b1ca5356caeeb0233b48a379
ArsenyBuntury <yeahwheel@gmail.com>
2022-03-24 13:40:58 +0300

    Prepare for heroku deploying

--- db/migrate/20220215144840_add_user_id_to_twits.rb
+++ db/migrate/20220215144840_add_user_id_to_twits.rb
@@ -1,5 +1,5 @@
 class AddUserIdToTwits < ActiveRecord::Migration[7.0]
   def change
-    add_reference :twits, :user, null: false, foreign_key: true, default: User.first.id
+    add_reference :twits, :user, null: false, foreign_key: true
   end
 end
