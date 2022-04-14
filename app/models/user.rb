395c52bf0017bc30fcf84b023554b3e5faebf4ca
ArsenyBuntury <yeahwheel@gmail.com>
2022-04-14 19:24:16 +0300

    Started working with followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,15 @@ class User < ApplicationRecord
   has_many :twits, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :microposts, dependent: :destroy
-
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   validates :password, presence: true
   validate :correct_old_password, on: :update, if: -> { password.present? }
   validates :password, confirmation: true, allow_blank: true
@@ -71,6 +79,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete << other_user
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
